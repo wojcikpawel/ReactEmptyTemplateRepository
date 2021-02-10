be43f0b6b82a523a00cafca3ca936b11287fd59c
wojcikpawel <wojcikpawel25@gmail.com>
2021-02-10 09:03:27 +0100

    Updates

---
+++ precache-manifest.b1df22a5ac271351ca62dd427954f82e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56bb3d2d022c340ad993383a9de5d191",
+    "url": "/ReactEmptyTemplateRepository/index.html"
+  },
+  {
+    "revision": "48c9d0157c78c830395e",
+    "url": "/ReactEmptyTemplateRepository/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "652a599d1a99efccd345",
+    "url": "/ReactEmptyTemplateRepository/static/js/2.a430f49c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReactEmptyTemplateRepository/static/js/2.a430f49c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48c9d0157c78c830395e",
+    "url": "/ReactEmptyTemplateRepository/static/js/main.f53acc72.chunk.js"
+  },
+  {
+    "revision": "764e0a4b34e448f637f6",
+    "url": "/ReactEmptyTemplateRepository/static/js/runtime-main.88cf144d.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/ReactEmptyTemplateRepository/static/media/logo.ee7cd8ed.svg"
+  }
+]);
